08db0b69a9a72c8311694898e57580f240a6f664
nublimr <nubliezrapat@gmail.com>
2019-07-04 09:39:11 +0700

    First Commit

---
+++ precache-manifest.b979fe705c8852faf98a20e10ff76825.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722f9101759a278ef41e6b8214b97682",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d544c10512516a2330b",
+    "url": "/static/css/main.cc268667.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "6d544c10512516a2330b",
+    "url": "/static/js/main.7a68081d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
